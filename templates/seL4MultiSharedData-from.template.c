6f6a641f1b52fc026da110e03a8879a4dec6e1af
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2016-07-28 15:42:55 +1000

    MultiSharedData connector no longer modifies ast
    The MultiSharedData connector establishes regions of shared memory
    between multiple clients connected to a single server. For each client,
    there is a region of shared memory between it and the server.
    
    Previously this was implemented by creating dataports by modifying the
    CAmkES specification, specifically by rewriting its AST to contain
    additional dataports. This uses the register_shared_variable function
    available to templates to establish shared memory.

--- templates/seL4MultiSharedData-from.template.c
+++ templates/seL4MultiSharedData-from.template.c
@@ -11,65 +11,40 @@
 #include <camkes/dataport.h>
 #include <utils/util.h>
 
-/*? macros.show_includes(me.from_instance.type.includes) ?*/
+/*? macros.show_includes(me.instance.type.includes) ?*/
 
-/*- set client_id = configuration[me.from_instance.name].get("%s_attributes" % me.from_interface.name) -*/
-/*- if client_id is none or re.match('"\\d+"$', client_id) is none -*/
-  /*? raise(Exception('%s.%s_attributes must be set to a number' % (me.from_instance.name, me.from_interface.name))) ?*/
+
+/*- set client_id = configuration[me.instance.name].get("%s_attributes" % me.interface.name) -*/
+/*- if client_id is none or re.match('\\d+$', client_id) is none -*/
+  /*? raise(Exception('%s.%s_attributes must be set to a number' % (me.instance.name, me.interface.name))) ?*/
 /*- endif -*/
 /*- set client_id = client_id.strip('"') -*/
-/*- set dataport_name = '%s_%s' % (me.from_interface.name, client_id) -*/
-/*- set dataport_type = configuration[me.from_instance.name].get("%s_dataport_type" % me.from_interface.name, 'Buf') -*/
 
-/*- set dataport_section = '%s_%s' % (me.from_interface.name, client_id) -*/
-
-/*# Create a fake instance in the assembly #*/
-/*- set port = lambda('x: AST.Port(type = x)')(str(dataport_type)) -*/
-/*- set dataport = lambda('x, y: AST.Dataport(x, y)')(port, dataport_name) -*/
-
-/*- do me.to_instance.type.dataports.append(dataport) -*/
-/*- do me.from_instance.type.dataports.append(dataport) -*/
-
-/*- set connection_type = lambda('x: AST.Connector(name=x, from_type="Dataport", to_type="Dataport")')(dataport_name) -*/
-/*- set connection = lambda('con_type, con_name, from_inst, from_inter, to_inst, to_inter: \
-                             AST.Connection(con_type, con_name, from_inst, from_inter, to_inst, to_inter)')
-                     (connection_type, dataport_name, me.from_instance, dataport, me.to_instance, dataport) -*/
-/*- do composition.connections.append(connection) -*/
-
-/*- set p = Perspective(dataport=dataport_name) -*/
-#define SHM_ALIGN (1 << 12)
-struct {
-    char content[ROUND_UP_UNSAFE(sizeof(/*? dataport_type ?*/),
-        PAGE_SIZE_4K)];
-} /*? p['dataport_symbol'] ?*/
-        __attribute__((aligned(SHM_ALIGN)))
-        __attribute__((section("shared_/*? dataport_section ?*/")))
-        __attribute__((externally_visible));
+/*- set shmem_size = configuration[me.instance.name].get("%s_shmem_size" % me.interface.name) -*/
+/*- set shmem_section = 'from_%s' % me.interface.name -*/
+/*- set shmem_symbol = 'from_%s_data' % me.interface.name -*/
+/*- set shmem_name = '%s%s' % (me.interface.name, suffix) -*/
 
 /*- if suffix is not defined -*/
   /*- set suffix = '' -*/
 /*- endif -*/
 
-volatile /*? dataport_type ?*/ * /*? me.from_interface.name ?*//*? suffix ?*/  =
-    (volatile /*? dataport_type ?*/ *) & /*? p['dataport_symbol'] ?*/;
+#define SHM_ALIGN (1 << 12)
+struct {
+    char content[ROUND_UP_UNSAFE(/*? shmem_size ?*/, PAGE_SIZE_4K)];
+} /*? shmem_symbol ?*/
+        __attribute__((aligned(SHM_ALIGN)))
+        __attribute__((section("shared_/*? shmem_symbol ?*/")))
+        __attribute__((externally_visible));
 
-/*- set isdata = lambda('x: isinstance(x, AST.Dataport)')(me.from_interface) -*/
-/*- if isdata -*/
-    /*- set p = Perspective(dataport=me.from_interface.name) -*/
-    struct {
-        char content[ROUND_UP_UNSAFE(sizeof(/*? show(me.from_interface.type) ?*/),
-            PAGE_SIZE_4K)];
-    } /*? p['dataport_symbol'] ?*/
-            __attribute__((aligned(SHM_ALIGN)))
-            __attribute__((section("shared_/*? me.from_interface.name ?*/")))
-            __attribute__((externally_visible));
-/*- endif -*/
+/*- do register_shared_variable('%s_%s_data' % (me.parent.name, client_id), shmem_symbol) -*/
 
+volatile void * /*? shmem_name ?*/ = (volatile void *) & /*? shmem_symbol ?*/;
 
-int /*? me.from_interface.name ?*/_wrap_ptr(dataport_ptr_t *p, void *ptr) {
+int /*? me.interface.name ?*/_wrap_ptr(dataport_ptr_t *p, void *ptr) {
     return -1;
 }
 
-void * /*? me.from_interface.name ?*/_unwrap_ptr(dataport_ptr_t *p) {
+void * /*? me.interface.name ?*/_unwrap_ptr(dataport_ptr_t *p) {
     return NULL;
 }
--- templates/seL4MultiSharedData-to.template.c
+++ templates/seL4MultiSharedData-to.template.c
@@ -11,52 +11,55 @@
 #include <camkes/dataport.h>
 #include <utils/util.h>
 
-/*? macros.show_includes(me.to_instance.type.includes) ?*/
+/*? macros.show_includes(me.instance.type.includes) ?*/
 
 /*# Enumerate all the incoming interfaces #*/
-/*- set dataports = [] -*/
+/*- set shmems = [] -*/
 /*- set client_ids = set() -*/
-/*- for id, c in enumerate(composition.connections) -*/
-    /*- if c.to_instance.name == me.to_instance.name and c.to_interface.name == me.to_interface.name -*/
-        /*- if c.type.name == me.type.name -*/
-            /*- set client_id = configuration[c.from_instance.name].get("%s_attributes" % c.from_interface.name) -*/
-            /*- if client_id is none or re.match('"\\d+"$', client_id) is none -*/
-              /*? raise(Exception('%s.%s_attributes must be set to a number' % (c.from_instance.name, c.from_interface.name))) ?*/
-            /*- endif -*/
-            /*- set client_id = client_id.strip('"') -*/
-            /*- if client_id not in client_ids -*/
-                /*- do client_ids.add(client_id) -*/
-                /*- set dataport_name = '%s_%s' % (c.from_interface.name, client_id) -*/
-                /*- set dataport_type = configuration[c.from_instance.name].get("%s_dataport_type" % c.from_interface.name, 'Buf') -*/
-                /*- set dataport_section = '%s_%s' % (me.to_interface.name, client_id) -*/
-                /*- set p = Perspective(dataport=dataport_name) -*/
-                #define SHM_ALIGN (1 << 12)
-                struct {
-                    char content[ROUND_UP_UNSAFE(sizeof(/*? dataport_type ?*/),
-                        PAGE_SIZE_4K)];
-                } /*? p['dataport_symbol'] ?*/
-                        __attribute__((aligned(SHM_ALIGN)))
-                        __attribute__((section("shared_/*? dataport_section ?*/")))
-                        __attribute__((externally_visible));
+/*- for c in me.parent.from_ends -*/
 
-                volatile /*? dataport_type ?*/ * /*? me.to_interface.name ?*/_buf_/*? client_id ?*/ =
-                    (volatile /*? dataport_type ?*/ *) & /*? p['dataport_symbol'] ?*/;
-                /*- do dataports.append( ('%s_buf_%s' % (me.to_interface.name, client_id), client_id) ) -*/
-            /*- else -*/
-                /* skipping /*? client_id ?*/ */
-            /*- endif -*/
-        /*- endif -*/
+    /*- set client_id = configuration[c.instance.name].get("%s_attributes" % c.interface.name) -*/
+    /*- if client_id is none or re.match('\\d+$', client_id) is none -*/
+        /*? raise(Exception('%s.%s_attributes must be set to a number' % (c.instance.name, c.interface.name))) ?*/
     /*- endif -*/
+    /*- set client_id = client_id.strip('"') -*/
+
+    /*- if client_id not in client_ids -*/
+        /*- do client_ids.add(client_id) -*/
+
+        /*- set shmem_size = configuration[c.instance.name].get("%s_shmem_size" % c.interface.name) -*/
+        /*- set shmem_section = 'to_%s_%s' % (me.interface.name, client_id) -*/
+        /*- set shmem_symbol = 'to_%s_%s_data' % (me.interface.name, client_id) -*/
+        /*- set shmem_name = "%s_buf_%s" % (me.interface.name, client_id) -*/
+
+        #define SHM_ALIGN (1 << 12)
+        struct {
+            char content[ROUND_UP_UNSAFE(/*? shmem_size ?*/, PAGE_SIZE_4K)];
+        } /*? shmem_symbol ?*/
+                __attribute__((aligned(SHM_ALIGN)))
+                __attribute__((section("shared_/*? shmem_section ?*/")))
+                __attribute__((externally_visible));
+
+        /*- do register_shared_variable('%s_%s_data' % (me.parent.name, client_id), shmem_symbol) -*/
+
+        volatile void * /*? shmem_name ?*/ = (volatile void *) & /*? shmem_symbol ?*/;
+
+        /*- do shmems.append((shmem_name, client_id)) -*/
+    /*- else -*/
+        /* skipping /*? client_id ?*/ */
+    /*- endif -*/
+
 /*- endfor -*/
 
-void */*? me.to_interface.name ?*/_buf(unsigned int client_id) {
-    /*- if len(dataports) == 0 -*/
+
+void * /*? me.interface.name ?*/_buf(unsigned int client_id) {
+    /*- if len(shmems) == 0 -*/
         return NULL;
     /*- else -*/
         switch (client_id) {
-            /*- for symbol, id in dataports -*/
+            /*- for symbol, id in shmems -*/
             case /*? id ?*/:
-                return (void*)/*? symbol ?*/;
+                return (void *) /*? symbol ?*/;
             /*- endfor -*/
         default:
             return NULL;
@@ -64,16 +67,16 @@ void */*? me.to_interface.name ?*/_buf(unsigned int client_id) {
     /*- endif -*/
 }
 
-unsigned int /*? me.to_interface.name ?*/_num_badges(void) {
-    return /*? len(dataports) ?*/;
+unsigned int /*? me.interface.name ?*/_num_badges(void) {
+    return /*? len(shmems) ?*/;
 }
 
-unsigned int /*? me.to_interface.name ?*/_enumerate_badge(unsigned int i) {
-    /*- if len(dataports) == 0 -*/
-        return -1;
+unsigned int /*? me.interface.name ?*/_enumerate_badge(unsigned int i) {
+    /*- if len(shmems) == 0 -*/
+       return -1;
     /*- else -*/
         switch (i) {
-            /*- for _, id in dataports -*/
+            /*- for _, id in shmems -*/
                 case /*? loop.index0 ?*/:
                     return /*? id ?*/;
             /*- endfor -*/
@@ -82,25 +85,3 @@ unsigned int /*? me.to_interface.name ?*/_enumerate_badge(unsigned int i) {
         }
     /*- endif -*/
 }
-
-/*- set isdata = lambda('x: isinstance(x, AST.Dataport)')(me.to_interface) -*/
-/*- if isdata -*/
-    /*- set p = Perspective(dataport=me.to_interface.name) -*/
-    struct {
-        char content[ROUND_UP_UNSAFE(sizeof(/*? show(me.to_interface.type) ?*/),
-            PAGE_SIZE_4K)];
-    } /*? p['dataport_symbol'] ?*/
-            __attribute__((aligned(SHM_ALIGN)))
-            __attribute__((section("shared_/*? me.to_interface.name ?*/")))
-            __attribute__((externally_visible));
-/*- endif -*/
-
-
-int /*? me.to_interface.name ?*/_wrap_ptr(dataport_ptr_t *p, void *ptr) {
-    return -1;
-}
-
-void * /*? me.to_interface.name ?*/_unwrap_ptr(dataport_ptr_t *p) {
-    return NULL;
-}
-
