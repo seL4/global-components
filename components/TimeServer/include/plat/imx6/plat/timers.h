e9385ee1fa97c4a1710c292558eb02679a7fcea6
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-09-26 14:23:04 +1000

    TimeServer: Added imx6 timer backened
    Added support for initialising an ltimer on the imx6 platform.

--- components/TimeServer/include/plat/imx6/plat/timers.h
+++ components/TimeServer/include/plat/imx6/plat/timers.h
@@ -9,10 +9,25 @@
  *
  * @TAG(DATA61_BSD)
  */
-
 #pragma once
 
-#define HARDWARE_TIMER_INTERFACES
+#define HARDWARE_TIMER_INTERFACES                                                   \
+    dataport Buf epit2_mem;                                                         \
+    consumes DataAvailable epit2_irq;                                               \
+	dataport Buf gpt_mem;                                                           \
+	consumes DataAvailable gpt_irq;
 #define HARDWARE_TIMER_ATTRIBUTES
-#define HARDWARE_TIMER_COMPOSITION
-#define HARDWARE_TIMER_CONFIG
+#define HARDWARE_TIMER_COMPOSITION                                                  \
+        component EPIT2 epit2;                                                      \
+        connection seL4HardwareMMIO epit2_mem(from epit2_mem, to epit2.mem);        \
+        connection seL4HardwareInterrupt epit2_irq(from epit2.irq, to epit2_irq);   \
+		component GPT gpt;                                                          \
+        connection seL4HardwareMMIO gpt_mem(from gpt_mem, to gpt.mem);              \
+        connection seL4HardwareInterrupt gpt_irq(from gpt.irq, to gpt_irq);
+#define HARDWARE_TIMER_CONFIG 												        \
+		epit2.mem_paddr <- mem_paddr;                                               \
+		epit2.mem_size  <-  mem_size;                                               \
+		epit2.irq_irq_number <- irq_irq_number;                                     \
+		gpt.mem_paddr <- mem_paddr;                                                 \
+        gpt.mem_size  <-  mem_size;                                                 \
+        gpt.irq_irq_number <- irq_irq_number;
