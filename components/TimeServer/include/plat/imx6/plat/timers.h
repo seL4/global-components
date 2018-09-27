cb12adf23ff8283c134be0c02586c1370fea3eb9
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-09-27 17:16:34 +1000

    components: imx6 TimeServer macros use dts vals
    Updated the imx6 timer server macros to pull memory and interrupt
    values from DTS bindings.

--- components/TimeServer/include/plat/imx6/plat/timers.h
+++ components/TimeServer/include/plat/imx6/plat/timers.h
@@ -12,22 +12,26 @@
 #pragma once
 
 #define HARDWARE_TIMER_INTERFACES                                                   \
-    dataport Buf epit2_mem;                                                         \
+    dataport Buf(16384) epit2_mem;                                                  \
     consumes DataAvailable epit2_irq;                                               \
-	dataport Buf gpt_mem;                                                           \
+	dataport Buf(16384) gpt_mem;                                                    \
 	consumes DataAvailable gpt_irq;
 #define HARDWARE_TIMER_ATTRIBUTES
 #define HARDWARE_TIMER_COMPOSITION                                                  \
-        component EPIT2 epit2;                                                      \
+        component EPIT epit2;                                                      \
         connection seL4HardwareMMIO epit2_mem(from epit2_mem, to epit2.mem);        \
         connection seL4HardwareInterrupt epit2_irq(from epit2.irq, to epit2_irq);   \
-		component GPT gpt;                                                          \
+        component GPT gpt;                                                          \
         connection seL4HardwareMMIO gpt_mem(from gpt_mem, to gpt.mem);              \
         connection seL4HardwareInterrupt gpt_irq(from gpt.irq, to gpt_irq);
-#define HARDWARE_TIMER_CONFIG 												        \
-		epit2.mem_paddr <- mem_paddr;                                               \
-		epit2.mem_size  <-  mem_size;                                               \
-		epit2.irq_irq_number <- irq_irq_number;                                     \
-		gpt.mem_paddr <- mem_paddr;                                                 \
-        gpt.mem_size  <-  mem_size;                                                 \
-        gpt.irq_irq_number <- irq_irq_number;
+#define HARDWARE_TIMER_CONFIG                                                                       \
+        epit2.dtb = dtb({"path" : "/soc/aips-bus@2000000/epit@20d4000"});                           \
+        gpt.dtb = dtb({"path" : "/soc/aips-bus@2000000/gpt@2098000"});                              \
+        epit2.mem_paddr <- epit2.dtb["reg"][0];                                                     \
+        epit2.mem_size  <- epit2.dtb["reg"][1];                                                     \
+        epit2.irq_irq_number <- epit2.dtb["interrupts"][1];                                         \
+        epit2.irq_spi_number <- epit2.dtb["interrupts"][0];                                             \
+        gpt.mem_paddr <- gpt.dtb["reg"][0];                                                         \
+        gpt.mem_size  <-  gpt.dtb["reg"][1];                                                        \
+        gpt.irq_irq_number <- gpt.dtb["interrupts"][1];                                             \
+        gpt.irq_spi_number <- gpt.dtb["interrupts"][0];
