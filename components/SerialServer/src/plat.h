dc88ac5c966cd38e67a8218338a1d1fb9bf61ab9
Damon Lee <Damon.Lee@data61.csiro.au>
2019-03-27 15:00:24 +1100

    SerialServer: Add platform IRQ function prototype
    This function is used as a wrapper to expose platform specific interrupt
    acknowledgement code.

--- components/SerialServer/src/plat.h
+++ components/SerialServer/src/plat.h
@@ -20,3 +20,5 @@ void plat_serial_interrupt(handle_char_fn handle_char);
 void plat_serial_putchar(int c);
 ssize_t plat_serial_read(void *buf, size_t buf_size, chardev_callback_t cb, void *token);
 ssize_t plat_serial_write(void *buf, size_t buf_size, chardev_callback_t cb, void *token);
+/* Definition located at plat/${KernelPlatform}/plat.c */
+int plat_serial_interrupt_acknowledge(void);
