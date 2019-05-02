23ea7c390247294cea5adcc6f79e3d1b5b29dfa8
Damon Lee <Damon.Lee@data61.csiro.au>
2019-05-02 13:48:24 +1000

    PCIConfigIO: Update to use new IO port functions
    This commit removes the old IO port functions defined by the
    seL4HardwareIOPort connector and replaces it with the libplatsupport IO
    port abstractions. The abstractions are connected to the libsel4camkes
    IO port functions.

--- components/PCIConfigIO/src/pci.c
+++ components/PCIConfigIO/src/pci.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, Data61
+ * Copyright 2019, Data61
  * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
  * ABN 41 687 119 230.
  *
@@ -15,6 +15,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <camkes.h>
+#include <camkes/io.h>
+
+static ps_io_port_ops_t io_ops;
 
 #define CONFIG_ADDR 0xcf8
 #define CONFIG_DATA 0xcfc
@@ -25,39 +28,68 @@ static void config_select(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int of
     uint32_t ldev = dev;
     uint32_t lfun = fun;
     uint32_t address = (lbus << 16) | (ldev << 11) | (lfun << 8) | (offset & 0xfc) | 0x80000000u;
-    config_address_out32(CONFIG_ADDR, address);
+    ps_io_port_out(&io_ops, CONFIG_ADDR, IOSIZE_32, address);
 }
 
 uint8_t pci_config_read8(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset) {
     config_select(bus, dev, fun, offset);
-    return config_data_in8(CONFIG_DATA + (offset & 3));
+    uint32_t result = 0;
+    uint32_t port = CONFIG_DATA + (offset & 3);
+    int error = ps_io_port_in(&io_ops, port, IOSIZE_8, &result);
+    if (error) {
+        return 0;
+    }
+
+    return (uint8_t) result;
 }
 
 uint16_t pci_config_read16(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset) {
     config_select(bus, dev, fun, offset);
-    return config_data_in16(CONFIG_DATA + (offset & 2));
+    uint32_t result = 0;
+    uint32_t port = CONFIG_DATA + (offset & 2);
+    int error = ps_io_port_in(&io_ops, port, IOSIZE_16, &result);
+    if (error) {
+        return 0;
+    }
+
+    return (uint16_t) result;
 }
 
 uint32_t pci_config_read32(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset) {
     config_select(bus, dev, fun, offset);
-    return config_data_in32(CONFIG_DATA);
+    uint32_t result = 0;
+    uint32_t port = CONFIG_DATA; 
+    int error = ps_io_port_in(&io_ops, port, IOSIZE_32, &result);
+    if (error) {
+        return 0;
+    }
+
+    return result;
 }
 
 void pci_config_write8(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset, uint8_t val) {
     config_select(bus, dev, fun, offset);
-    config_data_out8(CONFIG_DATA + (offset & 3), val);
+    uint32_t port = CONFIG_DATA + (offset & 3);
+    // Ignore the error, if any
+    ps_io_port_out(&io_ops, port, IOSIZE_8, val);
 }
 
 void pci_config_write16(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset, uint16_t val) {
     config_select(bus, dev, fun, offset);
-    config_data_out16(CONFIG_DATA + (offset & 2), val);
+    uint32_t port = CONFIG_DATA + (offset & 2);
+    // Ignore the error, if any
+    ps_io_port_out(&io_ops, port, IOSIZE_16, val);
 }
 
 void pci_config_write32(uint8_t bus, uint8_t dev, uint8_t fun, unsigned int offset, uint32_t val) {
     config_select(bus, dev, fun, offset);
-    config_data_out32(CONFIG_DATA, val);
+    uint32_t port = CONFIG_DATA;
+    // Ignore the error, if any
+    ps_io_port_out(&io_ops, port, IOSIZE_32, val);
 }
 
 void pre_init(void) {
     set_putchar(putchar_putchar);
+    int error = camkes_io_port_ops(&io_ops);
+    assert(!error);
 }
