8771a334028d95f0ef10347947ab09943f4a58aa
Damon Lee <Damon.Lee@data61.csiro.au>
2019-11-12 16:05:56 +1100

    SerialServer: Consolidate ARM platform code
    Following changes to the FDT query engine in camkes-tool, which allows
    the use of properties in the 'chosen' node in the devicetree, most of
    the boilerplate code for the ARM platforms can be consolidated to one
    location.

--- components/SerialServer/include/plat/arm_common/plat/serial.h
+++ components/SerialServer/include/plat/arm_common/plat/serial.h
@@ -11,16 +11,15 @@
  */
 #pragma once
 
-#define HARDWARE_SERIAL_INTERFACES                              \
-    emits Dummy dummy_source;                                   \
+#define HARDWARE_SERIAL_INTERFACES  \
+    emits Dummy dummy_source;       \
     consumes Dummy serial_dev;
 
 #define HARDWARE_SERIAL_ATTRIBUTES
 
-#define HARDWARE_SERIAL_COMPOSITION                                                     \
+#define HARDWARE_SERIAL_COMPOSITION                                                 \
         connection seL4DTBHardware serial_conn(from dummy_source, to serial_dev);
 
-#define HARDWARE_SERIAL_CONFIG                                      \
-        serial_dev.dtb = dtb({"path" : "/amba/serial@e0001000"});   \
+#define HARDWARE_SERIAL_CONFIG                               \
+        serial_dev.dtb = dtb({ "chosen" : "stdout-path" });  \
         serial_dev.generate_interrupts = 1;
-
