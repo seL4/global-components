a764892aa85c676b5d40464108273cae0dcba69a
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2020-03-27 18:03:28 +1100

    TimeServer,qemu-arm-virt: Only extract single irq
    Using the DTB query method would result in all 4 arm timer IRQs being
    allocated which cannot be fulfilled by the kernel. The timeserver only
    requires the physical EL1 timer IRQ for qemu-arm-virt.

--- components/TimeServer/include/plat/qemu-arm-virt/plat/timers.h
+++ components/TimeServer/include/plat/qemu-arm-virt/plat/timers.h
@@ -11,13 +11,20 @@
  */
 #pragma once
 
-#define HARDWARE_TIMER_INTERFACES                                                   \
-    consumes Dummy nv_timer;                                                        \
-    emits Dummy dummy_source;
+#define HARDWARE_TIMER_COMPONENT                                       \
+    component HWTimer {                                                \
+        hardware;                                                      \
+        emits TimerIRQ irq;                                          \
+    }
+
+#define HARDWARE_TIMER_INTERFACES                                      \
+    consumes TimerIRQ hwtimer_irq;
 #define HARDWARE_TIMER_ATTRIBUTES
-#define HARDWARE_TIMER_COMPOSITION                                                  \
-        connection seL4DTBHardware nv_timer_conn(from dummy_source, to nv_timer);
-#define HARDWARE_TIMER_CONFIG                                                       \
-        nv_timer.dtb = dtb({"path" : "/timer"});                           \
-        nv_timer.generate_interrupts = 1;
+#define HARDWARE_TIMER_COMPOSITION                                     \
+        component HWTimer hwtimer;                                    \
+        connection seL4HardwareInterrupt hwtimer_irq(from hwtimer.irq, \
+                                                      to hwtimer_irq);
+#define HARDWARE_TIMER_CONFIG                                            \
+    hwtimer.irq_irq_number = 30;                                        \
+    hwtimer.irq_irq_type = "arm";
 #define HARDWARE_TIMER_PLAT_INTERFACES
