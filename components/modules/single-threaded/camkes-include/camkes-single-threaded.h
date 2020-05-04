b99ce449a6af938172d73bb1df0fcfb72cceaac3
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2020-05-04 16:54:45 +1000

    Add single-threaded camkes module
    This module provide a way to define attributes and connections in a
    component to make it support single threaded execution. If using
    compatible connectors, the component will call different interface
    handler functions based on receiving RPC or notification messages.
    Because the component is single threaded, interfaces can assume that
    they have mutual exclusive access to global state until they finish
    processing.

---
+++ components/modules/single-threaded/camkes-include/camkes-single-threaded.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#define _VAR_STRINGIZE(...) #__VA_ARGS__
+#define VAR_STRINGIZE(...) _VAR_STRINGIZE(__VA_ARGS__)
+
+
+
+#define single_threaded_component() \
+    control; \
+    attribute int single_threaded = 1;
