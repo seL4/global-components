8b80368356e3d032d580150fd9788aecd5584c0c
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2020-05-22 15:41:49 +1000

    picotcp-socket-sync: Async extension

--- remote-drivers/picotcp-socket-sync/camkes-include/camkes-picotcp-socket-sync.h
+++ remote-drivers/picotcp-socket-sync/camkes-include/camkes-picotcp-socket-sync.h
@@ -21,6 +21,9 @@ import <picotcp-socket-sync.camkes>;
     uses PicoRecv name##_recv; \
     uses PicoSend name##_send; \
     emits Init name##_init1; \
+    uses VirtQueueDrv name##_tx; \
+    uses VirtQueueDrv name##_rx; \
+    dataport Buf name##_dma_pool; \
     consumes Init name##_init2;
 
 
@@ -30,6 +33,9 @@ import <picotcp-socket-sync.camkes>;
     provides PicoRecv name##_recv; \
     provides PicoSend name##_send; \
     emits Init name##_init1; \
+    uses VirtQueueDev name##_tx; \
+    uses VirtQueueDev name##_rx; \
+    dataport Buf name##_dma_pool; \
     consumes Init name##_init2;
 
 
@@ -41,6 +47,11 @@ import <picotcp-socket-sync.camkes>;
     connection seL4PicoServer client##_##client_name##server##_##server_name##_recv(from client.client_name##_recv, to server.server_name##_recv); \
     connection seL4PicoServer client##_##client_name##server##_##server_name##_send(from client.client_name##_send, to server.server_name##_send); \
     connection PicotcpSocketSyncClientInit client##_##client_name##_client_init(from client.client_name##_init1, to client.client_name##_init2); \
+    component VirtQueueInit client_name##d0; \
+    component VirtQueueInit client_name##d1; \
+    connection seL4VirtQueues client_name##_virtq_conn0(to client_name##d0.init, from client.client_name##_tx, from server.server_name##_tx); \
+    connection seL4VirtQueues client_name##_virtq_conn1(to client_name##d1.init, from client.client_name##_rx, from server.server_name##_rx); \
+    connection seL4DMASharedData client_name##_dma(from client.client_name##_dma_pool, to server.server_name##_dma_pool);
 
 
 #define picotcp_socket_sync_server_configurations(name) \
@@ -49,7 +60,12 @@ import <picotcp-socket-sync.camkes>;
 #define picotcp_socket_sync_client_configurations(client, client_name, packet_size) \
     client.client_name##_recv_shmem_size = packet_size; \
     client.client_name##_send_shmem_size = packet_size; \
-    client##_##client_name##_client_init.connection_name = VAR_STRINGIZE(client_name);
+    client##_##client_name##_client_init.connection_name = VAR_STRINGIZE(client_name); \
+    client_name##_dma.size = 0x1000000; \
+    client_name##_virtq_conn0.queue_length = 1024; \
+    client_name##_virtq_conn1.queue_length = 1024; \
+    client_name##_dma.controller = VAR_STRINGIZE(client.client_name##_dma_pool);
+
 
 
 
--- remote-drivers/picotcp-socket-sync/server/picoserver_socket.h
+++ remote-drivers/picotcp-socket-sync/server/picoserver_socket.h
@@ -13,10 +13,35 @@
 #pragma once
 
 #include <pico_socket.h>
+#include <picoserver_event.h>
+
+typedef struct picoserver_buffer_in_progress {
+    tx_msg_t *buf;
+    void *cookie_save;
+} picoserver_buffer_in_progress_t;
+
+typedef struct picoserver_socket_async {
+    /* Number of UDP packets queued, or 1 if any TCP data is pending */
+    size_t rx_pending;
+    /* Queued empty packets to receive data into */
+    tx_msg_t *rx_pending_queue;
+    tx_msg_t *rx_pending_queue_end;
+
+    /* Pending packets to send */
+    tx_msg_t *tx_pending_queue;
+    tx_msg_t *tx_pending_queue_end;
+
+} picoserver_socket_async_t;
+
 
 typedef struct picoserver_socket {
+    int protocol;
     int client_id;
     int socket_fd;
     struct pico_socket *socket;
     uint16_t events;
+
+    /* This is set if the client sets the socket as async */
+    picoserver_socket_async_t *async_transport;
 } picoserver_socket_t;
+
