bfc09b3f4aeea83d53ce5c98aeb2441e7cc534d3
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2020-07-15 11:06:53 +1000

    BenchUtiliz: Tweak measurement control process
    This changes the behavior of the component so that the idle thread is
    running continuously and the start and stop procedures sample values of
    the idle thread's state that are continuously changing. This allows the
    start and stop procedures to run on a different thread with a higher
    priority and reduce the chance of measurement errors that can arise when
    the idle thread gets a command to start executing, but it isn't
    scheduled until partway through the measurement and records late start
    times and likewise for finishing. The consequence of this change is that
    when a measurement isn't being taken the idle thread will still run when
    the system would otherwise be idle. We assume that a system using this
    component is purpose configured for measuring idle time and doesn't care
    about minimizing execution when measurements aren't being taken.

--- components/BenchUtiliz/src/benchUtiliz.c
+++ components/BenchUtiliz/src/benchUtiliz.c
@@ -29,53 +29,88 @@ volatile bool flag = 0;
 
 void pre_init(void)
 {
+    sel4bench_init();
+    int err = bench_to_reg_callback(&count_idle, NULL);
+    if (err) {
+        ZF_LOGE("Failed to register callback handler");
+    }
+    bench_from_emit();
+
     set_putchar(serial_putchar_putchar);
 }
 
+uint64_t ccount = 0;
+uint64_t prev, start, ts, overflows;
+uint64_t idle_ccount_start;
+uint64_t idle_overflow_start;
 
 void getchar_handler(void)
 {
     seL4_Word badge;
+    uint64_t total, idle;
     while (1) {
         seL4_Wait(serial_getchar_notification(), &badge);
         char ch = serial_getchar_buf->buf[serial_getchar_buf->head];
         serial_getchar_buf->head = (serial_getchar_buf->head + 1) % sizeof(serial_getchar_buf->buf);
         switch (ch) {
         case 'a':
-            if (!flag) {
-                flag = 1;
-                bench_from_emit();
-            }
+            idle_start();
             break;
         case 'b':
-            flag = 0;
+            idle_stop(&total, &idle);
+            printf("idle/tot: %llu/%llu, idle proportion: %f%%\n", idle, total, ((double) idle / (double) total) * 100);
             break;
         default:
-            assert(!"What are you doing?");
             break;
         }
     }
 }
 
-uint64_t ccount = 0, tot = 0;
-uint64_t prev, start, ts;
+
+void idle_start(void)
+{
+    if (!flag) {
+        flag = 1;
+        printf("Measurment starting...\n");
+        start = (uint64_t)sel4bench_get_cycle_count();
+        idle_ccount_start = ccount;
+        idle_overflow_start = overflows;
+
+    }
+}
+
+
+void idle_stop(uint64_t *total_ret, uint64_t *idle_ret)
+{
+    flag = 0;
+    uint64_t total = ((uint64_t)sel4bench_get_cycle_count()) - start;
+    if ((overflows - idle_overflow_start) > 0) {
+        ZF_LOGE("Cycle counter overflowed during measurement. Stats may not be accurate");
+    }
+    total += ULONG_MAX * (overflows - idle_overflow_start);
+    uint64_t idle_total = ccount - idle_ccount_start;
+    *total_ret = total;
+    *idle_ret = idle_total;
+}
+
 
 void count_idle(UNUSED void *arg)
 {
-    printf("Measurment starting...\n");
-    sel4bench_init();
-    start = (uint64_t)sel4bench_get_cycle_count();
-    prev = start;
+    prev = sel4bench_get_cycle_count();
+    ccount = 0;
+    overflows = 0;
 
-    while (flag) {
+    while (1) {
         ts = (uint64_t)sel4bench_get_cycle_count();
         uint64_t diff;
 
-        /* handle overflow */
+        /* Handle overflow: This thread needs to run at least 2 times
+           within any ULONG_MAX cycles period to detect overflows.
+           This also assumes that the cycle counter overflows every ULONG_MAX
+           cycles which may not be true for all platforms.*/
         if (ts < prev) {
             diff = ULONG_MAX - prev + ts + 1;
-            start = ts;
-            tot += ULONG_MAX + ts + 1;
+            overflows++;
         } else {
             diff = ts - prev;
         }
@@ -87,13 +122,10 @@ void count_idle(UNUSED void *arg)
         }
         prev = ts;
     }
-    tot += ts - start;
-    printf("idle/tot: %llu/%llu, idle proportion: %f%%\n", ccount, tot, ((double) ccount / (double) tot) * 100);
 }
 
 int run(void)
 {
-    bench_to_reg_callback(&count_idle, NULL);
     getchar_handler();
 
     return 0;
