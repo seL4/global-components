c505951068c12772cc15ba48c162e0fbd4a5278b
Damon Lee <Damon.Lee@data61.csiro.au>
2021-01-13 12:42:12 +1100

    Ethdriver: Add support for zynqmp
    This commit adds support for zynqmp to the Ethdriver component.

---
+++ components/Ethdriver/include/plat/zynqmp/plat/eth_devices.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+#pragma once
+
+#include <camkes-fdt-bind-driver.h>
+
+#define HARDWARE_ETHERNET_EXTRA_IMPORTS
+
+#define HARDWARE_ETHERNET_COMPONENT
+
+#define HARDWARE_ETHERNET_INTERFACES                                    \
+    consumes Dummy EthDriver;                                           \
+    emits Dummy dummy_source;                                           \
+    fdt_bind_drivers_interfaces(["/amba/ethernet@ff0e0000"]);
+
+#define HARDWARE_ETHERNET_COMPOSITION                                             \
+    connection seL4DTBHardwareThreadless ethdriver_conn(from dummy_source,        \
+                                                        to EthDriver);            \
+    fdt_bind_driver_connections();
+
+#define HARDWARE_ETHERNET_CONFIG                                        \
+    EthDriver.dtb = dtb({ "path" : "/amba/ethernet@ff0e0000" });        \
+    EthDriver.generate_interrupts = 1;
