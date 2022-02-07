87b8f623b3dd26dc5de68e511d374a0a8cf953b2
Joonas Onatsu <joonas.onatsu@unikie.com>
2022-02-08 01:58:22 +0200

    TimeServer,bcm2711: Add GPIO MMIO mapping
    The new BCM2711 GPIO driver in "util_libs" was failing during
    runtime, due to device untyped page being created for the GPIO
    MMIO register bank. This affected the BCM2711 serial drivers,
    as they use the GPIO driver for explicit GPIO pin initialization.
    
    This fix only helps when using the SerialServer component, and
    without using it, the frame mapping will still be missing.
    TODO: find better way to implement the fix.
    
    Signed-off-by: Joonas Onatsu <joonasx@ssrc.tii.ae>

---
+++ components/SerialServer/src/plat/bcm2711/plat.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2022, Technology Innovation Institute
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <autoconf.h>
+#include <sel4/sel4.h>
+#include <camkes.h>
+#include <utils/util.h>
+#include <platsupport/irq.h>
+#include <sel4utils/sel4_zf_logif.h>
+
+#include "../../plat.h"
+#include "../../serial.h"
+
+void plat_post_init(ps_irq_ops_t *irq_ops)
+{
+    ps_irq_t irq_info = { .type = PS_INTERRUPT, .irq = { .number = DEFAULT_SERIAL_INTERRUPT }};
+    irq_id_t serial_irq_id = ps_irq_register(irq_ops, irq_info, serial_server_irq_handle, NULL);
+    ZF_LOGF_IFERR(serial_irq_id < 0, "Failed to register irq for serial");
+}
