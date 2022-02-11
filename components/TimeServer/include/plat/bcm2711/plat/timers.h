616629fa2496ee3174280b141a89a41961cbad48
Markku Ahvenjärvi <markkux@ssrc.tii.ae>
2022-02-11 14:17:00 +0200

    TimeServer,bcm2711: Add device tree support
    Change RPi4 TimeServer component to be generated from device tree.
    
    Signed-off-by: Markku Ahvenjärvi <markkux@ssrc.tii.ae>

--- components/TimeServer/include/plat/bcm2711/plat/timers.h
+++ components/TimeServer/include/plat/bcm2711/plat/timers.h
@@ -6,36 +6,13 @@
  */
 #pragma once
 
-#define HARDWARE_TIMER_COMPONENT                                       \
-    component HWTimer {                                                \
-        hardware;                                                      \
-        dataport Buf(4096) mem;                                        \
-        emits TimerIRQ irq;                                            \
-    }
-
-#define HARDWARE_TIMER_INTERFACES                                      \
-    dataport Buf(4096) systimer_mem;                                   \
-    dataport Buf(4096) sp804_mem;                                      \
-    consumes TimerIRQ systimer_irq;                                    \
-    consumes TimerIRQ sp804_irq;
-
+#define HARDWARE_TIMER_INTERFACES                                        \
+    emits Dummy dummy_source;                                            \
+    consumes Dummy system_timer;
 #define HARDWARE_TIMER_ATTRIBUTES
 #define HARDWARE_TIMER_COMPOSITION                                       \
-        component HWTimer systimer;                                      \
-        connection seL4HardwareMMIO systimer_mem(from systimer_mem,      \
-                                                 to systimer.mem);       \
-        connection seL4HardwareInterrupt systimer_irq(from systimer.irq, \
-                                                      to systimer_irq);  \
-        component HWTimer sp804timer;                                    \
-        connection seL4HardwareMMIO sp804_mem(from sp804_mem,            \
-                                              to sp804timer.mem);        \
-        connection seL4HardwareInterrupt sp804_irq(from sp804timer.irq,  \
-                                                   to sp804_irq);
+        connection seL4DTBHardware system_timer_conn(from dummy_source, to system_timer);
 #define HARDWARE_TIMER_CONFIG                                            \
-    systimer.mem_paddr = 0xfe003000;                                     \
-    systimer.mem_size = 0x1000;                                          \
-    systimer.irq_irq_number = 97;                                        \
-    sp804timer.mem_paddr = 0xfe00b000;                                   \
-    sp804timer.mem_size = 0x1000;                                        \
-    sp804timer.irq_irq_number = 64;
+        system_timer.dtb = dtb({"path" : "/soc/timer@7e003000"});        \
+        system_timer.generate_interrupts = 1;
 #define HARDWARE_TIMER_PLAT_INTERFACES
