ab981dbc69cf2d44a38b48970e2c3e72916e3cc7
Lucy <lucyparker544@gmail.com>
2022-03-14 13:54:15 +1100

    Support for imx8mm architecture
    Signed-off-by: Lucy <lucyparker544@gmail.com>

---
+++ components/Ethdriver/include/plat/imx8mm-evk/plat/eth_devices.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+#pragma once
+
+#include <camkes-fdt-bind-driver.h>
+
+#define HARDWARE_ETHERNET_EXTRA_IMPORTS
+
+#define HARDWARE_ETHERNET_COMPONENT
+
+#define HARDWARE_ETHERNET_INTERFACES                                                \
+    consumes Dummy EthDriver;                                                       \
+    consumes Dummy ocotp;                                                           \
+    consumes Dummy iomux;                                                           \
+    consumes Dummy ccm;                                                             \
+    consumes Dummy analog;                                                          \
+    consumes Dummy gpio1;                                                           \
+    emits Dummy dummy_source; \
+    fdt_bind_drivers_interfaces(["/ethernet@30be0000"]);
+
+#define HARDWARE_ETHERNET_COMPOSITION                                               \
+    connection seL4DTBHardwareThreadless ethernet_conn(from dummy_source, to EthDriver);      \
+    connection seL4DTBHardwareThreadless ocotp_conn(from dummy_source, to ocotp);             \
+    connection seL4DTBHardwareThreadless iomux_conn(from dummy_source, to iomux);             \
+    connection seL4DTBHardwareThreadless ccm_conn(from dummy_source, to ccm);                 \
+    connection seL4DTBHardwareThreadless analog_conn(from dummy_source, to analog);           \
+    connection seL4DTBHardwareThreadless gpio1_conn(from dummy_source, to gpio1); \
+    fdt_bind_driver_connections();
+
+#define HARDWARE_ETHERNET_CONFIG                                                    \
+    EthDriver.dtb = dtb({ "path" : "/ethernet@30be0000" });     \
+    EthDriver.generate_interrupts = 1;                                              \
+    ocotp.dtb = dtb({ "path" : "/ocotp-ctrl@30350000" });            \
+    iomux.dtb = dtb({ "path" : "/pinctrl@30330000" });           \
+    ccm.dtb = dtb({ "path" : "/ccm@30380000" });                \
+    analog.dtb = dtb({ "path" : "/anatop@30360000" });          \
+    gpio1.dtb = dtb({"path" : "/gpio@30200000"});
