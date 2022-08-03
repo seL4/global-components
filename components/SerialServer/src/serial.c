d0508943ce1d3c73706c9d9419c08883036b80e2
Andy Bui <andy.bui@unsw.edu.au>
2022-08-03 05:30:55 +0000

    VM_Arm: Support console multiplexing
    Signed-off-by: Andy Bui <andy.bui@unsw.edu.au>

--- components/SerialServer/src/serial.c
+++ components/SerialServer/src/serial.c
@@ -21,90 +21,48 @@
 #include <platsupport/irq.h>
 #include "serial.h"
 #include "plat.h"
-#include "server_virtqueue.h"
 
 #define ESCAPE_CHAR '@'
-#define MAX_CLIENTS 12
-#define CLIENT_OUTPUT_BUFFER_SIZE 4096
-
-/* TODO: have the MultiSharedData template generate a header with these */
-void getchar_emit(unsigned int id) WEAK;
-seL4_Word getchar_enumerate_badge(unsigned int id) WEAK;
-unsigned int getchar_num_badges() WEAK;
-void *getchar_buf(unsigned int id) WEAK;
-void *processed_batch_buf(unsigned int id) WEAK;
-void *raw_batch_buf(unsigned int id) WEAK;
-int getchar_largest_badge(void) WEAK;
-
-typedef struct getchar_buffer {
-    uint32_t head;
-    uint32_t tail;
-    char buf[4096 - 8];
-} client_buffer_t;
-
-compile_time_assert(getchar_buf_sized, sizeof(client_buffer_t) == sizeof(Buf));
+#define NUM_CLIENTS ARRAY_SIZE(serial_layout)
+#define BUFSIZE 4088
 
 typedef struct getchar_client {
-    unsigned int client_id;
-    volatile client_buffer_t *buf;
-    uint32_t last_head;
-} getchar_client_t;
+    virtqueue_device_t *recv_queue;
+    virtqueue_driver_t *send_queue;
+} client_t;
+
+static client_t *clients = NULL;
+char gather_buf[BUFSIZE];
 
 static ps_io_ops_t io_ops;
 
 static int last_out = -1;
-
-static uint8_t output_buffers[MAX_CLIENTS * 2][CLIENT_OUTPUT_BUFFER_SIZE];
-static int output_buffers_used[MAX_CLIENTS * 2] = { 0 };
+static uint8_t output_buffers[NUM_CLIENTS][BUFSIZE];
+static int output_buffers_used[NUM_CLIENTS] = { 0 };
 static uint16_t output_buffer_bitmask = 0;
-
 static int done_output = 0;
-
 static int has_data = 0;
 
-static int num_getchar_clients = 0;
-static getchar_client_t *getchar_clients = NULL;
-
 /* We predefine output colours for clients */
-const char *all_output_colours[2][MAX_CLIENTS] = {
-    {
-        /* Processed streams */
-        ANSI_COLOR(RED),
-        ANSI_COLOR(GREEN),
-        ANSI_COLOR(BLUE),
-        ANSI_COLOR(MAGENTA),
-        ANSI_COLOR(YELLOW),
-        ANSI_COLOR(CYAN),
-        ANSI_COLOR(RED, BOLD)
-        ANSI_COLOR(GREEN, BOLD),
-        ANSI_COLOR(BLUE, BOLD),
-        ANSI_COLOR(MAGENTA, BOLD),
-        ANSI_COLOR(YELLOW, BOLD),
-        ANSI_COLOR(CYAN, BOLD),
-    },
-    {
-        /* Raw streams */
-        ANSI_COLOR2(RED, WHITE),
-        ANSI_COLOR2(GREEN, WHITE),
-        ANSI_COLOR2(BLUE, WHITE),
-        ANSI_COLOR2(MAGENTA, WHITE),
-        ANSI_COLOR2(YELLOW, WHITE),
-        ANSI_COLOR2(CYAN, WHITE),
-        ANSI_COLOR2(RED, WHITE, BOLD)
-        ANSI_COLOR2(GREEN, WHITE, BOLD),
-        ANSI_COLOR2(BLUE, WHITE, BOLD),
-        ANSI_COLOR2(MAGENTA, WHITE, BOLD),
-        ANSI_COLOR2(YELLOW, WHITE, BOLD),
-        ANSI_COLOR2(CYAN, WHITE, BOLD),
-    },
+const char *all_output_colours[NUM_CLIENTS] = {
+    /* Processed streams */
+    ANSI_COLOR(RED),
+    ANSI_COLOR(GREEN),
+    ANSI_COLOR(BLUE),
+    ANSI_COLOR(MAGENTA),
+    ANSI_COLOR(YELLOW),
+    ANSI_COLOR(CYAN),
+    ANSI_COLOR(RED, BOLD)
+    ANSI_COLOR(GREEN, BOLD),
+    ANSI_COLOR(BLUE, BOLD),
+    ANSI_COLOR(MAGENTA, BOLD),
+    ANSI_COLOR(YELLOW, BOLD),
+    ANSI_COLOR(CYAN, BOLD)
 };
-
-#define COLOUR_INDEX_TO_STYLE(x) ((x) / (MAX_CLIENTS - 1))
-#define COLOUR_INDEX_TO_SLOT(x)  ((x) % MAX_CLIENTS)
 
 static void flush_buffer(int b)
 {
-    const char *col = all_output_colours[COLOUR_INDEX_TO_STYLE(b)][COLOUR_INDEX_TO_SLOT(b)];
+    const char *col = all_output_colours[b];
     int i;
     if (output_buffers_used[b] == 0) {
         return;
@@ -151,7 +109,7 @@ static bool flush_buffer_line(int b)
         return 0;
     }
     if (b != last_out) {
-        printf("%s%s", COLOR_RESET, all_output_colours[COLOUR_INDEX_TO_STYLE(b)][COLOUR_INDEX_TO_SLOT(b)]);
+        printf("%s%s", COLOR_RESET, all_output_colours[b]);
         last_out = b;
     }
     int i;
@@ -194,9 +152,9 @@ static int active_multiclients = 0;
 static int try_coalesce_output()
 {
     size_t length = 0;
-    size_t used[MAX_CLIENTS * 2] = { 0 };
+    size_t used[NUM_CLIENTS] = { 0 };
     size_t n_used = 0;
-    for (size_t i = 0; i < MAX_CLIENTS * 2; i++) {
+    for (size_t i = 0; i < NUM_CLIENTS; i++) {
         if (output_buffers_used[i] > 0) {
             used[n_used++] = i;
             if (length == 0 || length > output_buffers_used[i]) {
@@ -261,7 +219,7 @@ static void internal_putchar(int b, int c)
         /* Test for special case: multiple clients outputting the EXACT SAME THING. */
         coalesce_status = try_coalesce_output();
     }
-    if (output_buffers_used[b] == CLIENT_OUTPUT_BUFFER_SIZE) {
+    if (output_buffers_used[b] == BUFSIZE) {
         /* Since we're violating contract anyway (flushing in the
          * middle of someone else's line), flush all buffers, so the
          * fastpath can be used again. */
@@ -273,15 +231,15 @@ static void internal_putchar(int b, int c)
         }
         while (!is_done) {
             is_done = 1;
-            for (i = 0; i < MAX_CLIENTS * 2; i++) {
+            for (i = 0; i < NUM_CLIENTS; i++) {
                 if (flush_buffer_line(i)) {
                     is_done = 0;
                 }
             }
         }
         /* Flush the rest, if necessary. */
-        if (output_buffers_used[b] == CLIENT_OUTPUT_BUFFER_SIZE) {
-            for (i = 0; i < MAX_CLIENTS * 2; i++) {
+        if (output_buffers_used[b] == BUFSIZE) {
+            for (i = 0; i < NUM_CLIENTS; i++) {
                 if (i != b) {
                     flush_buffer(i);
                 }
@@ -291,7 +249,7 @@ static void internal_putchar(int b, int c)
          * it's probably going to overflow again, so let's avoid
          * that. */
         if (last_out != b) {
-            printf("%s%s", COLOR_RESET, all_output_colours[COLOUR_INDEX_TO_STYLE(b)][COLOUR_INDEX_TO_SLOT(b)]);
+            printf("%s%s", COLOR_RESET, all_output_colours[b]);
             last_out = b;
         }
     } else if ((index >= 1 && is_newline(buffer + index - 1) && coalesce_status == -1)
@@ -310,33 +268,76 @@ static void internal_putchar(int b, int c)
     error = serial_unlock();
 }
 
-static void internal_raw_putchar(int id, int c)
+static void serial_notify_free_send(virtqueue_driver_t *queue)
 {
-    getchar_client_t *client = &getchar_clients[id];
-    uint32_t next_tail = (client->buf->tail + 1) % sizeof(client->buf->buf);
-    if (next_tail == client->buf->head) {
-        /* full */
-        return;
-    }
-    uint32_t last_read_head = client->buf->head;
-    client->buf->buf[client->buf->tail] = (uint8_t)c;
-    /* no synchronize in here as we assume TSO */
-    client->buf->tail = next_tail;
-    __sync_synchronize();
-    if (last_read_head != client->last_head) {
-        getchar_emit(client->client_id);
-        client->last_head = last_read_head;
+    void *buf = NULL;
+    size_t buf_size = 0, wr_len = 0;
+    vq_flags_t flag;
+    virtqueue_ring_object_t handle;
+    while (virtqueue_get_used_buf(queue, &handle, &wr_len)) {
+        while (camkes_virtqueue_driver_gather_buffer(queue, &handle, &buf, &buf_size, &flag) >= 0) {
+            /* Clean up and free the buffer we allocated */
+            camkes_virtqueue_buffer_free(queue, buf);
+        }
     }
 }
 
-static int statemachine = 1;
+static int serial_notify_recv(int client, virtqueue_device_t *queue)
+{
+    int err;
+    void *buf = NULL;
+    size_t buf_size = 0;
+    vq_flags_t flag;
+    virtqueue_ring_object_t handle;
+
+    while (virtqueue_get_available_buf(queue, &handle)) {
+        size_t len = virtqueue_scattered_available_size(queue, &handle);
+        if (camkes_virtqueue_device_gather_copy_buffer(queue, &handle, gather_buf, len) < 0) {
+            ZF_LOGW("Dropping data for client %d: Can't gather vq buffer.", client);
+            continue;
+        }
+
+        /* Print to serial */
+        for (size_t i = 0; i < len; i++) {
+            char ch = gather_buf[i];
+            if (ch == '\n') {
+                internal_putchar(client, '\r');
+            }
+            internal_putchar(client, ch);
+        }
+        queue->notify();
+    }
+}
+
+/* This is called whenever one of our virtqueues get notified */
+void serial_wait_callback(void)
+{
+    /* Walk through all recv virtqueues and poll for data */
+    for (int i = 0; i < NUM_CLIENTS; i++) {
+        if (clients[i].recv_queue && VQ_DEV_POLL(clients[i].recv_queue)) {
+            serial_notify_recv(i, clients[i].recv_queue);
+        }
+        
+        if (clients[i].send_queue && VQ_DRV_POLL(clients[i].send_queue)) {
+            serial_notify_free_send(clients[i].send_queue);
+        }
+    }
+}
+
+static void internal_raw_putchar(int id, int c)
+{
+    /* This is so overkill we need a better solution than this */
+    int err = camkes_virtqueue_driver_scatter_send_buffer(clients[id].send_queue, (void *) &c, 1);
+    ZF_LOGE_IF(err < 0, "Unknown error while enqueuing char for client %d", id);
+    clients[id].send_queue->notify();
+}
 
 static void give_client_char(uint8_t c)
 {
     if (active_client >= 0) {
         internal_raw_putchar(active_client, c);
     } else if (active_client == -1) {
-        for (int i = 0; i < MAX_CLIENTS * 2; i++) {
+        for (int i = 0; i < NUM_CLIENTS; i++) {
             if ((active_multiclients & BIT(i)) == BIT(i)) {
                 internal_raw_putchar(i, c);
             }
@@ -344,10 +345,11 @@ static void give_client_char(uint8_t c)
     }
 }
 
+static int statemachine = 1;
 static void handle_char(uint8_t c)
 {
-    /* If there are no getchar clients, then we return immediately */
-    if (!getchar_num_badges) {
+    /* If there are no clients, then we return immediately */
+    if (!NUM_CLIENTS) {
         return;
     }
 
@@ -398,11 +400,11 @@ static void handle_char(uint8_t c)
                    "\r\n          0: no debugging"
                    "\r\n          1: debug multi-input mode output coalescing"
                    "\r\n          2: debug flush_buffer_line"
-                   "\r\n", ESCAPE_CHAR, num_getchar_clients - 1);
+                   "\r\n", ESCAPE_CHAR, NUM_CLIENTS - 1);
             statemachine = 1;
             break;
         default:
-            if (c >= '0' && c < '0' + num_getchar_clients) {
+            if (c >= '0' && c < '0' + NUM_CLIENTS) {
                 last_out = -1;
                 int client = c - '0';
                 printf(COLOR_RESET "\r\nSwitching input to %d\r\n", client);
@@ -416,7 +418,7 @@ static void handle_char(uint8_t c)
         }
         break;
     case 3:
-        if (c >= '0' && c < '0' + num_getchar_clients) {
+        if (c >= '0' && c < '0' + NUM_CLIENTS) {
             printf(COLOR_RESET "%s%d", (active_multiclients != 0 ? "," : ""), (c - '0'));
             active_multiclients |= BIT(c - '0');
             last_out = -1;
@@ -443,7 +445,7 @@ static void timer_callback(void *data)
         char succeeded = 0;
         while (!is_done) {
             is_done = 1;
-            for (i = 0; i < MAX_CLIENTS * 2; i++) {
+            for (i = 0; i < NUM_CLIENTS; i++) {
                 if (flush_buffer_line(i)) {
                     succeeded = 1;
                     is_done = 0;
@@ -451,7 +453,7 @@ static void timer_callback(void *data)
             }
         }
         if (!succeeded) {
-            for (i = 0; i < MAX_CLIENTS * 2; i++) {
+            for (i = 0; i < NUM_CLIENTS; i++) {
                 flush_buffer(i);
             }
         }
@@ -460,7 +462,6 @@ static void timer_callback(void *data)
 }
 
 seL4_CPtr timeout_notification(void);
-
 int run(void)
 {
     seL4_CPtr notification = timeout_notification();
@@ -498,84 +499,79 @@ void pre_init(void)
     error = camkes_io_ops(&io_ops);
     ZF_LOGF_IF(error, "Failed to initialise IO ops");
 
-    // Initialize the serial port
+    /* Initialize the serial port */
     plat_pre_init(&io_ops);
     set_putchar(serial_putchar);
-    /* query what getchar clients exist */
-    if (getchar_num_badges) {
-        num_getchar_clients = getchar_num_badges();
-        getchar_clients = calloc(num_getchar_clients, sizeof(getchar_client_t));
-        for (int i = 0; i < num_getchar_clients; i++) {
-            unsigned int badge = getchar_enumerate_badge(i);
-            assert(badge <= num_getchar_clients);
-            getchar_clients[i].client_id = badge;
-            getchar_clients[i].buf = getchar_buf(badge);
-            getchar_clients[i].last_head = -1;
-        }
-    }
+
     plat_post_init(&(io_ops.irq_ops));
+
     /* Start regular heartbeat of 500ms */
     timeout_periodic(0, 500000000);
     error = serial_unlock();
+    ZF_LOGF_IF(error, "Failed to unlock serial");
+}
+
+int virtqueue_init(void)
+{
+    int err;
+    /* Query what clients exist */
+    clients = calloc(NUM_CLIENTS, sizeof(client_t));
+
+    for (int i = 0; i < NUM_CLIENTS; i++) {
+        virtqueue_driver_t *vq_send;
+        virtqueue_device_t *vq_recv;
+
+        vq_recv = malloc(sizeof(*vq_recv));
+        if (!vq_recv) {
+            ZF_LOGE("Unable to alloc recv camkes-virtqueue for client: %d", i);
+            err = -1;
+            goto cleanup;
+        }
+
+        vq_send = malloc(sizeof(*vq_send));
+        if (!vq_send) {
+            ZF_LOGE("Unable to alloc send camkes-virtqueue for client: %d", i);
+            err = -1;
+            goto cleanup;
+        }
+
+        /* Initialise read virtqueue */
+        err = camkes_virtqueue_device_init(vq_recv, serial_layout[i].recv_id);
+        if (err) {
+            ZF_LOGE("Unable to initialise serial server recv virtqueue for client %d", i);
+            goto cleanup;
+        }
+
+        /* Initialise write virtqueue */
+        err = camkes_virtqueue_driver_init(vq_send, serial_layout[i].send_id);
+        if (err) {
+            ZF_LOGE("Unable to initialise serial server send virtqueue for client %d", i);
+            goto cleanup;
+        }
+
+        clients[i].recv_queue = vq_recv;
+        clients[i].send_queue = vq_send;       
+    }
+
+    return 0;
+
+cleanup:
+    for (int i = 0; i < num_clients; i++) {
+        if (clients[i].recv_queue) {
+            free(clients[i].recv_queue);
+        }
+        if (clients[i].send_queue) {
+            free(clients[i].send_queue);
+        }
+    }
+
+    return err;
 }
 
 void post_init(void)
 {
     if (num_registered_virtqueue_channels > 0) {
         int res = virtqueue_init();
-        if (res) {
-            ZF_LOGE("Serial server does not support read and write virtqueues");
-        }
+        ZF_LOGE_IF(res, "Serial server does not support read and write virtqueues");
     }
-}
-
-seL4_Word processed_putchar_get_sender_id(void) WEAK;
-void processed_putchar_putchar(int c)
-{
-    seL4_Word n = processed_putchar_get_sender_id();
-    if (c == '\n') {
-        internal_putchar(n, '\r');
-    }
-    internal_putchar((int)n, c);
-}
-
-seL4_Word raw_putchar_get_sender_id(void) WEAK;
-void raw_putchar_putchar(int c)
-{
-    seL4_Word n = raw_putchar_get_sender_id();
-    internal_putchar((int)n + MAX_CLIENTS, c);
-}
-
-seL4_Word processed_batch_get_sender_id(void) WEAK;
-void processed_batch_batch(void)
-{
-    seL4_Word n = processed_batch_get_sender_id();
-    client_buffer_t *putchar_buf = processed_batch_buf(n);
-    while (putchar_buf->head != putchar_buf->tail) {
-        char ch = putchar_buf->buf[putchar_buf->head];
-        if (ch == '\n') {
-            internal_putchar(n, '\r');
-        }
-        internal_putchar((int)n, ch);
-        putchar_buf->head = (putchar_buf->head + 1) % sizeof(putchar_buf->buf);
-    }
-}
-
-seL4_Word raw_batch_get_sender_id(void) WEAK;
-void raw_batch_batch(void)
-{
-    seL4_Word n = raw_batch_get_sender_id();
-    client_buffer_t *putchar_buf = raw_batch_buf(n);
-    while (putchar_buf->head != putchar_buf->tail) {
-        char ch = putchar_buf->buf[putchar_buf->head];
-        internal_putchar((int)n + MAX_CLIENTS, ch);
-        putchar_buf->head = (putchar_buf->head + 1) % sizeof(putchar_buf->buf);
-    }
-}
-
-/* We had to define at least one function in the getchar RPC procedure
- * so now we need to implement it even though it is not used */
-void getchar_foo(void)
-{
-    assert(!"should not be reached");
 }
