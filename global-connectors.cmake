e482500bc852bf236d32ff9ec5070e130d72e2a1
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-08-14 13:40:07 +0100

    Bump minimum CMake version
    See seL4/seL4@e7bb62d2 for rationale.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- global-connectors.cmake
+++ global-connectors.cmake
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-cmake_minimum_required(VERSION 3.8.2)
+cmake_minimum_required(VERSION 3.16.0)
 
 CAmkESAddImportPath(interfaces plat_interfaces/${KernelPlatform})
 
