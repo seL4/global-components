dd6a1ec846c422da85fdd6900df182095f9b7425
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-06-12 08:50:47 +1000

    trivial: cmake style update
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- global-connectors.cmake
+++ global-connectors.cmake
@@ -12,133 +12,116 @@ CAmkESAddImportPath(interfaces plat_interfaces/${KernelPlatform})
 CAmkESAddTemplatesPath(templates)
 
 # Connector templates with FROM and TO only
-foreach(connector IN ITEMS seL4GlobalAsynchCallback seL4MessageQueue
-                           seL4RPCOverMultiSharedData
-)
-    DeclareCAmkESConnector(
-        ${connector} FROM ${connector}-from.template.c TO ${connector}-to.template.c
-    )
+foreach(connector IN ITEMS seL4GlobalAsynchCallback seL4MessageQueue seL4RPCOverMultiSharedData)
+  DeclareCAmkESConnector(${connector} FROM ${connector}-from.template.c TO
+                         ${connector}-to.template.c)
 endforeach()
 
 # Connector templates with FROM, FROM_HEADER, TO and TO_HEADER
 foreach(connector IN ITEMS seL4RPCCallSignal seL4RPCDataport seL4RPCDataportSignal seL4RPCNoThreads
-                           seL4GPIOServer seL4Ethdriver
-)
-    DeclareCAmkESConnector(
-        ${connector}
-        FROM
-        ${connector}-from.template.c
-        FROM_HEADER
-        ${connector}-from.template.h
-        TO
-        ${connector}-to.template.c
-        TO_HEADER
-        ${connector}-to.template.h
-    )
+                           seL4GPIOServer seL4Ethdriver)
+  DeclareCAmkESConnector(
+    ${connector}
+    FROM
+    ${connector}-from.template.c
+    FROM_HEADER
+    ${connector}-from.template.h
+    TO
+    ${connector}-to.template.c
+    TO_HEADER
+    ${connector}-to.template.h)
 endforeach()
 
 # Specific connector templates not fitting with the schemes above
-DeclareCAmkESConnector(
-    seL4SharedDataWithCaps FROM seL4SharedDataWithCaps.template.c TO
-    seL4SharedDataWithCaps.template.c
-)
+DeclareCAmkESConnector(seL4SharedDataWithCaps FROM seL4SharedDataWithCaps.template.c TO
+                       seL4SharedDataWithCaps.template.c)
 
 DeclareCAmkESConnector(
-    seL4GlobalAsynch
-    FROM
-    seL4GlobalAsynch-from.template.c
-    FROM_HEADER
-    seL4GlobalAsynch-from.template.h
-    TO
-    seL4GlobalAsynch-to.template.c
-)
+  seL4GlobalAsynch
+  FROM
+  seL4GlobalAsynch-from.template.c
+  FROM_HEADER
+  seL4GlobalAsynch-from.template.h
+  TO
+  seL4GlobalAsynch-to.template.c)
 
 DeclareCAmkESConnector(
-    seL4TimeServer
-    FROM
-    seL4RPCCallSignal-from.template.c
-    FROM_HEADER
-    seL4RPCCallSignal-from.template.h
-    TO
-    seL4RPCCallSignal-to.template.c
-    TO_HEADER
-    seL4RPCCallSignal-to.template.h
-)
+  seL4TimeServer
+  FROM
+  seL4RPCCallSignal-from.template.c
+  FROM_HEADER
+  seL4RPCCallSignal-from.template.h
+  TO
+  seL4RPCCallSignal-to.template.c
+  TO_HEADER
+  seL4RPCCallSignal-to.template.h)
 
 DeclareCAmkESConnector(
-    seL4SerialServer
-    FROM
-    seL4RPCDataportSignal-from.template.c
-    FROM_HEADER
-    seL4RPCDataportSignal-from.template.h
-    TO
-    seL4RPCDataportSignal-to.template.c
-)
+  seL4SerialServer
+  FROM
+  seL4RPCDataportSignal-from.template.c
+  FROM_HEADER
+  seL4RPCDataportSignal-from.template.h
+  TO
+  seL4RPCDataportSignal-to.template.c)
 
 DeclareCAmkESConnector(
-    seL4RPCCallSignalNoThreads
-    FROM
-    seL4RPCCallSignal-from.template.c
-    FROM_HEADER
-    seL4RPCCallSignal-from.template.h
-    TO
-    seL4RPCCallSignal-to.template.c
-    TO_HEADER
-    seL4RPCCallSignal-to.template.h
-)
+  seL4RPCCallSignalNoThreads
+  FROM
+  seL4RPCCallSignal-from.template.c
+  FROM_HEADER
+  seL4RPCCallSignal-from.template.h
+  TO
+  seL4RPCCallSignal-to.template.c
+  TO_HEADER
+  seL4RPCCallSignal-to.template.h)
 
 DeclareCAmkESConnector(
-    seL4PicoServerSignal
-    FROM
-    seL4RPCCallSignal-from.template.c
-    FROM_HEADER
-    seL4RPCCallSignal-from.template.h
-    TO
-    seL4RPCCallSignal-to.template.c
-    TO_HEADER
-    seL4RPCCallSignal-to.template.h
-)
+  seL4PicoServerSignal
+  FROM
+  seL4RPCCallSignal-from.template.c
+  FROM_HEADER
+  seL4RPCCallSignal-from.template.h
+  TO
+  seL4RPCCallSignal-to.template.c
+  TO_HEADER
+  seL4RPCCallSignal-to.template.h)
 
 DeclareCAmkESConnector(
-    seL4RPCDataportNoThreads
-    FROM
-    seL4RPCDataport-from.template.c
-    FROM_HEADER
-    seL4RPCDataport-from.template.h
-    TO
-    seL4RPCDataport-to.template.c
-    TO_HEADER
-    seL4RPCDataport-to.template.h
-)
+  seL4RPCDataportNoThreads
+  FROM
+  seL4RPCDataport-from.template.c
+  FROM_HEADER
+  seL4RPCDataport-from.template.h
+  TO
+  seL4RPCDataport-to.template.c
+  TO_HEADER
+  seL4RPCDataport-to.template.h)
 
 DeclareCAmkESConnector(
-    seL4PicoServer
-    FROM
-    seL4RPCDataport-from.template.c
-    FROM_HEADER
-    seL4RPCDataport-from.template.h
-    TO
-    seL4RPCDataport-to.template.c
-    TO_HEADER
-    seL4RPCDataport-to.template.h
-)
+  seL4PicoServer
+  FROM
+  seL4RPCDataport-from.template.c
+  FROM_HEADER
+  seL4RPCDataport-from.template.h
+  TO
+  seL4RPCDataport-to.template.c
+  TO_HEADER
+  seL4RPCDataport-to.template.h)
 
-DeclareCAmkESConnector(
-    seL4GlobalAsynchHardwareInterrupt TO seL4GlobalAsynchHardwareInterrupt.template.c
-)
+DeclareCAmkESConnector(seL4GlobalAsynchHardwareInterrupt TO
+                       seL4GlobalAsynchHardwareInterrupt.template.c)
 
-DeclareCAmkESConnector(
-    seL4DTBHardwareThreadless FROM empty.c TO seL4DTBHardwareThreadless.template.c
-)
+DeclareCAmkESConnector(seL4DTBHardwareThreadless FROM empty.c TO
+                       seL4DTBHardwareThreadless.template.c)
 
 DeclareCAmkESConnector(seL4DTBHWThreadless TO seL4DTBHardwareThreadless.template.c)
 
 DeclareCAmkESConnector(
-    seL4VirtQueues
-    FROM
-    seL4VirtQueues-from.template.c
-    FROM_HEADER
-    seL4VirtQueues-from.template.h
-    TO
-    empty.c
-)
+  seL4VirtQueues
+  FROM
+  seL4VirtQueues-from.template.c
+  FROM_HEADER
+  seL4VirtQueues-from.template.h
+  TO
+  empty.c)
